b634890973b1de3ed790f243f6e324ff86f5a40a
ByMagnuz <brayanestrada027@gmail.com>
2024-06-11 23:23:30 -0600

    Delete chat directory

--- chat/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .models import Mensaje
-
-def home(request):
-    return render(request, 'home.html')
-
-def messages(request):
-    mensajes = Mensaje.objects.all().order_by('id')
-    return render(request, 'messages.html', {'mensajes': mensajes})
