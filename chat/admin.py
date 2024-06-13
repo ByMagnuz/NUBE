98e89490b6c8d10563d145cccacc639b43bb916d
ByMagnuz <brayanestrada027@gmail.com>
2024-06-13 12:43:41 -0600

    commit final final

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Carrera, Persona, Mensaje  # Importa tus modelos
 
-# Register your models here.
+admin.site.register(Carrera)  # Registra el modelo Carrera
+admin.site.register(Persona)  # Registra el modelo Persona
+admin.site.register(Mensaje)  # Registra el modelo Mensaje
